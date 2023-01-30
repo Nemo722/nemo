2c290326373b2380162e630e13eb87ca1e3dd62f
minglong.zhao <minglong.zhao@infaith.com.cn>
2023-01-30 11:16:15 +0800

    测试Controller

---
+++ src/main/java/com/nemo/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.nemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author mingLong.zhao
+ */
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test(@RequestParam("a") String a) {
+        return a;
+    }
+}
